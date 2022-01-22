64f7ad06efcbb3a84ccd24011fe07e03603f70f2
Igor <tuea1982@gmail.com>
2022-01-22 19:12:43 +0200

    HW_13

---
+++ HW_13.js
@@ -0,0 +1,39 @@
+let array = [15, 25, 50, 13, -2, 17, 121];
+
+
+console.log('for Each');
+
+array.forEach((n, i, a) => console.log(`${ '#' + n}`));
+
+
+
+console.log('My for Each');
+
+function myForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+        callback(array[index]);
+        
+    }
+}
+
+function callback(element) {
+    console.log(element / -1) ;
+}
+
+myForEach(array,callback);
+
+
+const arr = [];
+arr.concat = array.reverse();
+console.log(arr);
+
+console.log('my Map:');
+
+function myMap(arr, callback){
+    for (let index = 0; index < array.length; index++) {
+        callback(array[index] * 2);
+        
+    }
+}
+
+myMap(arr, callback);
